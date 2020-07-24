c729c22b51600f8b70d03e8f0cb3269a735e6ff5
Jakub Kozniewski <jkozniewski@gmail.com>
2020-07-24 18:49:53 +0200

    scroll

--- arduino/adafruit14SegTest1/adafruit14SegTest1.ino
+++ arduino/adafruit14SegTest1/adafruit14SegTest1.ino
@@ -5,7 +5,10 @@
 Adafruit_AlphaNum4 alpha4 = Adafruit_AlphaNum4();
 
 float value = 0.0;
-char buff[11];
+char buff[32];
+String txt = "COVID-19";
+int scrollOffset = 0;
+int frameLength = 4;
 
 void setup() {
   
@@ -16,12 +19,14 @@ void setup() {
   alpha4.clear();
   alpha4.writeDisplay();
 
-  for( int i = 0; i < 11; i++ ){
+  for( int i = 0; i < 32; i++ ){
     buff[i] = ' ';
   }
-  
- 
 
+  for( int i = 0; i < txt.length(); i++ ){
+    buff[i] = txt.charAt(i);
+  }
+  
 }
 
 void loop() {
@@ -30,20 +35,34 @@ void loop() {
   int b = map( s, -100, 100, 0, 15 );
   
   //https://arduinobasics.blogspot.com/2019/05/sprintf-function.html
-  sprintf(buff,"%4d", int(value)); //
+  //sprintf(buff,"%4d", int(value));//
 
   for( int i = 0; i < 4; i++ ){
-    alpha4.writeDigitAscii(i, buff[i]);
+
+    if( scrollOffset < 0 ){
+      alpha4.writeDigitAscii(i + (scrollOffset*-1), buff[i]);
+    }else{
+      alpha4.writeDigitAscii(i, buff[i + scrollOffset]);
+    }
+    
   }
 
-  
-
-  alpha4.setBrightness(b);
+  //alpha4.setBrightness(b);
   alpha4.writeDisplay();
 
   value += 1;
+
+  if( scrollOffset < int(txt.length()) ){ // ????
+    scrollOffset++;
+  }else{
+    scrollOffset = -(frameLength-1);
+  }
+
+  //Serial.println( scrollOffset );
+  //Serial.println( txt.length() );
   
-  delay(5);
+  
+  delay(100);
   
   
 }
