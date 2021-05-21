72a01a6f0957f706c5f88719f4659150eb37b3a7
NCNCNCNCNC <64972937+NCNCNCNCNC@users.noreply.github.com>
2021-05-21 19:21:29 +0200

    wersja softu pod foto

---
+++ arduino/wiatrakFirmware2_foto/Timeline.h
@@ -0,0 +1,213 @@
+#include <Easing.h>
+// 
+
+typedef float (*KeyEasing)(float t, float b, float c, float d);
+
+typedef struct _keyframe {
+    float val;
+    unsigned long dur;
+    KeyEasing ease;
+    unsigned long pause;
+} Keyframe;
+
+
+class Timeline {
+
+  Keyframe* keyframes;
+  int keyframeIndex = 0;
+  int keyframeCount;
+
+  float currentValue = 0.0;
+  float initialValue = 0.0;
+  float destinationValue = 0.0;
+  float previousValue = 0.0;
+  
+  unsigned long timer;
+  unsigned long duration;
+  unsigned long keyDuration;
+
+  unsigned long pauseDuration;
+  unsigned long pauseTimer;
+
+  bool running = false;
+  bool paused = false;
+  bool finished = false;
+  
+  
+  public:
+  // ----------------------------------------------------
+  
+  void setup(){
+    
+  }
+
+  void play( Keyframe* _key, int _keyCount ){
+
+    play( _key, _keyCount, 0.0 ); 
+    
+  }
+
+  void play( Keyframe* _key, int _keyCount, float _initialVal ){
+
+    //Serial.println( "----- ANIM START -----" );
+    
+    keyframes = _key;
+    keyframeCount = _keyCount;
+    keyframeIndex = 0;
+
+    initialValue = _initialVal;
+    previousValue = initialValue;
+    destinationValue = keyframes[ keyframeIndex ].val;
+    keyDuration = keyframes[ keyframeIndex ].dur;
+    
+    timer = millis();
+    running = true;
+    finished = false;
+    paused = false;
+    
+  }
+
+  void stop(){
+
+    running = false;
+    
+  }
+
+  void reset(){
+
+    running = false;
+    finished = false;
+    keyframeIndex = 0;
+    currentValue = 0;
+    previousValue = initialValue;
+    destinationValue = keyframes[ keyframeIndex ].val;
+    keyDuration = keyframes[ keyframeIndex ].dur;
+
+    //Serial.println( "----- ANIM STOPPED -----" );
+    
+  }
+
+  void nextKeyframe(){
+
+    currentValue = destinationValue; // HACK to avoid rounding erros;
+
+    if( keyframeIndex < keyframeCount-1 ){
+
+      keyframeIndex ++;
+      
+      previousValue = currentValue;
+      destinationValue = keyframes[ keyframeIndex ].val;
+      keyDuration = keyframes[ keyframeIndex ].dur;
+
+      timer = millis();
+      
+      
+    }else{
+
+      if( running ){
+         //Serial.println( "----------- END --------------" );
+         finished = true;
+      }
+      
+      running = false;
+      
+    }
+    
+  }
+
+  void update(){
+
+    if( running ){
+
+      
+
+      if( paused ){
+
+        if( millis() - pauseTimer <= keyframes[ keyframeIndex ].pause ){
+          return;
+        }else{
+          paused = false;
+          nextKeyframe();
+        }
+        
+      }
+    
+      double p = getCurrentKeyProgress();
+  
+      if( millis() <= timer + keyDuration ){
+   
+         currentValue = keyframes[ keyframeIndex ].ease( p, previousValue, destinationValue - previousValue, 1.0 );
+         //Serial.println( currentValue );
+         
+      }else{
+
+         if(keyframes[ keyframeIndex ].pause > 0){
+            pauseTimer = millis();
+            paused = true;
+         }else{
+            nextKeyframe();
+         }
+         
+      }
+
+      //
+
+    }else{
+
+      finished = false;
+      
+    }
+    
+  }
+
+  float getCurrentRemappedValue( float startVal, float endVal  ){
+
+    float val = keyframes[ keyframeIndex ].ease( getCurrentKeyProgress(), startVal, endVal - startVal, 1.0 );
+    return val;
+    
+  }
+
+  double getCurrentKeyProgress(){
+    return min( double(millis() - timer) / double( keyDuration ), 1.0 );
+  }
+
+  int getCurrentKeyIndex(){
+    return keyframeIndex;
+  }
+
+  float getDestinationValue(){
+    return destinationValue;
+  }
+
+  float getCurrentValue() {
+    return currentValue;
+  }
+
+  bool isRunning(){
+    return running;
+  }
+
+  bool isFinished(){
+    return finished;
+  }
+
+  bool isPaused(){
+    return paused;
+  }
+
+        // DEBUG
+//      Serial.print( keyframeIndex );
+//      Serial.print( " " );
+//      Serial.print( keyDuration );
+//      Serial.print( " " );
+//      Serial.print( previousValue );
+//      Serial.print( " " );
+//      Serial.print( destinationValue );
+//      Serial.print( " " );
+//      Serial.print( p );
+//      Serial.print( " " );
+//      Serial.println( currentValue );
+      //
+
+  
+};
---
+++ arduino/wiatrakFirmware2_foto/wiatrakFirmware2_foto.ino
@@ -0,0 +1,246 @@
+// WIATRAK FRIRMWARE v2
+
+#include <Servo.h>
+#include "Timeline.h"
+#include "DisplayController.h"
+
+DisplayController displayController;
+
+Servo motor;
+Servo servo;
+
+#define MOTOR_PIN 6
+#define SERVO_PIN 3
+#define MIN_MOTOR_SPEED 45
+#define MAX_MOTOR_SPEED 140
+#define MOTOR_INIT_VAL 30
+#define MIN_SERVO_MS 500
+#define MAX_SERVO_MS 2500
+
+#define VALUES_COUNT 7
+#define DISEASE_COUNT 5
+#define INFO_COUNT 4
+
+char *diseases[] = {"MEASLES", "COVID-19", "TUBERCULOSIS", "SWINE FLU", "MERS-COV"};
+float deaths[DISEASE_COUNT][VALUES_COUNT] = {
+  {1000000, 535300, 454000, 164000, 12000, 8978, 0},
+  {36375, 142261, 162935, 160842, 272129, 414975, 0},
+  {2530000, 2977000, 3500000, 1600000, 1450000, 1800000, 0},
+  {99, 1369, 3056, 1515, 195, 51, 0},
+  {6, 100, 277, 268, 47, 59, 0}
+};
+
+float deathValBounds[DISEASE_COUNT][2] = {
+  {8978, 1000000},
+  {36375, 414975},
+    {1450000, 3500000},
+  {51, 3056},
+  {6, 277}
+};
+char *dates[DISEASE_COUNT][VALUES_COUNT] = {
+  {"/1996/", "/2000/", "/2004/", "/2008/", "/2012/", "/2016/", "///"},
+    {"03/2020", "05/2020", "07/2020", "09/2020", "11/2020", "01/2021", "///"},
+  {"/1990/", "/1995/", "/2000/", "/2005/", "/2010/", "/2015/", "///"},
+  {"05/2009", "08/2009", "11/2009", "02/2010", "05/2010", "08/2010", "///"},
+  {"/2012/", "/2013/", "/2014/", "/2015/", "/2016/", "/2017/", "///"}
+};
+float angles [] = {0, 0, 36, 72, 108, 144, 180};
+Keyframe mainKeyframeBuffer[VALUES_COUNT];
+Timeline mainTimeline;
+Keyframe infoKeyframeBuffer[INFO_COUNT];
+Timeline infoTimeline;
+
+// wartosc, nazwa, data, wartosc
+int infoPauses[] = { 3000, 3000, 2000, 3000 };
+
+
+int currentDiseaseIndex = 0;
+int currentDeathIndex = 0;
+float value = 0.0;
+float ang = 0.0;
+
+unsigned long transitionDuration = 8000;
+unsigned long returnDuration = 8000;
+unsigned long pauseDuration = 0; // ustawiane w setup na bazie pauz w info
+unsigned long transitonTimer = 0;
+
+int displayBlinkRate = 50;
+
+boolean sequenceStarted = false;
+
+void setup() {
+
+  //pinMode(A0, INPUT);
+  pinMode(MOTOR_PIN, OUTPUT);
+  pinMode(SERVO_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  //digitalWrite(MOTOR_PIN, LOW);
+  //delay(10);
+
+  motor.attach(MOTOR_PIN);
+  motor.write(MOTOR_INIT_VAL); // nie moze byc zero
+  servo.attach(SERVO_PIN, MIN_SERVO_MS, MAX_SERVO_MS);
+  servo.write(0);
+
+  Serial.begin(9600);
+
+  displayController.setup();
+  displayController.setTextAlign( ALIGN_CENTER );
+  displayController.writeText( "HELLO123" );
+  displayController.updateImmediate(); //??
+
+  digitalWrite( LED_BUILTIN, LOW );
+  delay(8000);
+  digitalWrite( LED_BUILTIN, HIGH );
+
+
+  pauseDuration = getPauseDuration();
+  setupSequence(currentDiseaseIndex);
+  startSequence();
+
+}
+
+unsigned long getPauseDuration() {
+  unsigned long sum = 0;
+  for ( int i = 0; i < INFO_COUNT; i ++ ) {
+    sum += infoPauses[i];
+  }
+  return sum;
+}
+
+void setupSequence(int srcIndex) {
+
+  for ( int i = 0; i < VALUES_COUNT; i ++ ) {
+
+    if ( i < VALUES_COUNT - 1 ) {
+      mainKeyframeBuffer[ i ] = { deaths[srcIndex][i], transitionDuration, Easing::easeInOutCubic, pauseDuration };
+    } else {
+      mainKeyframeBuffer[ i ] = { deaths[srcIndex][i], returnDuration, Easing::easeInOutCubic, 0 };
+    }
+
+  }
+
+}
+
+void setInfoSequence() {
+
+  for ( int i = 0; i < INFO_COUNT; i ++ ) {
+
+    infoKeyframeBuffer[ i ] = { 100, infoPauses[i], Easing::easeInOutCubic, 0 };
+
+  }
+
+}
+
+void startSequence() {
+
+  sequenceStarted = true;
+  mainTimeline.play( mainKeyframeBuffer, VALUES_COUNT );
+
+}
+
+int getCurrentServoPos() {
+
+  float val = mainTimeline.getCurrentRemappedValue(
+                angles[mainTimeline.getCurrentKeyIndex()],
+                angles[(mainTimeline.getCurrentKeyIndex() + 1) % (VALUES_COUNT)]
+              );
+
+  return int( val );
+
+}
+
+int getCurrentMotorSpeed() {
+
+  int motorSpeed = map(mainTimeline.getCurrentValue(), deathValBounds[currentDiseaseIndex][0], deathValBounds[currentDiseaseIndex][1], MIN_MOTOR_SPEED, MAX_MOTOR_SPEED);
+  return motorSpeed;
+
+}
+
+void update() {
+
+  mainTimeline.update();
+  infoTimeline.update();
+
+  displayController.update();
+
+  servo.write( getCurrentServoPos() );
+
+  int motorSpeed = (mainTimeline.getCurrentKeyIndex() < VALUES_COUNT - 1) ? getCurrentMotorSpeed() : MIN_MOTOR_SPEED;
+  motor.write( getCurrentMotorSpeed() );
+
+  if ( mainTimeline.isPaused() ) { // PAUZA
+
+    if ( !infoTimeline.isRunning() ) {
+
+      setInfoSequence();
+      infoTimeline.play( infoKeyframeBuffer, 4 );
+
+    } else {
+
+      switch ( infoTimeline.getCurrentKeyIndex() ) {
+
+        case 0: // WARTOSC
+          displayController.setTextAlign( ALIGN_CENTER );
+          displayController.writeNumber( long( mainTimeline.getCurrentValue() ) );
+          displayController.setBlinkRate( displayBlinkRate );
+          break;
+
+        case 1: // NAZWA
+          displayController.setTextAlign( ALIGN_FREE );
+          displayController.writeText( diseases[currentDiseaseIndex] );
+          displayController.setBlinkRate( 0 );
+          break;
+
+        case 2: // DATA
+          displayController.setTextAlign( ALIGN_CENTER );
+          displayController.writeText( dates[currentDiseaseIndex][mainTimeline.getCurrentKeyIndex()] );
+          displayController.setBlinkRate( 0 );
+          break;
+
+        case 3: // WARTOSC
+          displayController.setTextAlign( ALIGN_CENTER );
+          displayController.writeNumber( long( mainTimeline.getCurrentValue() ) );
+          displayController.setBlinkRate( displayBlinkRate );
+          break;
+
+      }
+
+    }
+
+  } else { // PRZEJSCIE
+
+    if ( mainTimeline.getCurrentKeyIndex() < VALUES_COUNT - 1 ) { // JESLI KOLEJNE DANE O ZGONACH
+      displayController.setBlinkRate( 0 );
+      displayController.setTextAlign( ALIGN_CENTER );
+      displayController.writeNumber( long( mainTimeline.getCurrentValue() ) );
+    } else { // JESLI ANIMACJA POWROTU
+      displayController.setTextAlign( ALIGN_FREE );
+      displayController.writeText( "   " );
+      // displayController.writeText( "*/*/*/*/" );
+      displayController.setBlinkRate( 0 );
+    }
+
+
+  }
+
+  if ( mainTimeline.isFinished() ) {
+
+    currentDiseaseIndex = ((currentDiseaseIndex + 1) % DISEASE_COUNT);
+    setupSequence( currentDiseaseIndex );
+    mainTimeline.play( mainKeyframeBuffer, VALUES_COUNT, deathValBounds[currentDiseaseIndex][0] );
+
+
+  }
+
+}
+
+
+
+void loop() {
+
+  update();
+
+}
+//EOF.
